9c5694bce8b62e1b4a26c1a7002a645dfb1173ae
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-10-17 14:39:00 -0500

    made the navbar effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+// nav bar effect on scroll
 
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("static", window.scrollY > 0);
+});
