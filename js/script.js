d81e508c0d82f070e750f552ea5e9d3bc0adac0a
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-10-25 23:27:50 -0500

    responsive nav bar done, working on the hamburguer menu

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
   navbar.classList.toggle("static", window.scrollY > 0);
 });
+
+//ham menu effect-responsive design
+const hamIcon = document.getElementById("ham-icon");
+const hamMenu = document.getElementById("ham-menu");
+const navbarLinks = document.querySelector(".navbar__links");
+hamMenu.addEventListener("click", () => {
+  hamIcon.innerHTML = "close";
+  hamMenu.classList.toggle("active");
+  navbarLinks.classList.toggle("active");
+});
