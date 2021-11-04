d29df7b7085b4799153a11f4084878a22c129aea
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-11-03 23:38:37 -0500

    add some description

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ const hamMenu = document.getElementById("ham-menu");
 const navbarLinks = document.querySelector(".navbar__links");
 const hamMenuItems = document.querySelectorAll(".navbar__links a");
 const sendBtn = document.querySelector(".send-btn");
+const eyes = document.querySelectorAll(".eye");
 
 function verifyActiveMenu() {
   if (hamMenu.classList.contains("active")) {
@@ -31,5 +32,18 @@ hamMenuItems.forEach((item) => {
     hamMenu.classList.remove("active");
     navbarLinks.classList.remove("active");
     verifyActiveMenu();
+  });
+});
+//eyes
+eyes.forEach((eye) => {
+  window.addEventListener("mousemove", (event) => {
+    let x = eye.left + eye.x / 2;
+    let y = eye.top + eye.y / 2;
+    let rad = Math.atan2(event.x - x, event.x - y);
+    let rot = rad * (180 / Math.PI) * -1 + 180;
+    eye.style.webkit = "rotate(" + rot + "deg)";
+    eye.style.moz = "rotate(" + rot + "deg)";
+    eye.style.ms = "rotate(" + rot + "deg)";
+    eye.style.transform = "rotate(" + rot + "deg)";
   });
 });
