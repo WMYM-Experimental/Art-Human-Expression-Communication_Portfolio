8a13d369b126dd9312a6e309de88dd2e5126e501
Washington Yandun <washington.yandun06@gmail.com>
2021-12-16 23:26:51 -0500

    forget eyes idea

--- js/script.js
+++ js/script.js
@@ -34,18 +34,3 @@ hamMenuItems.forEach((item) => {
     verifyActiveMenu();
   });
 });
-
-/* eyes followers (working on it)
-eyes.forEach((eye) => {
-  window.addEventListener("mousemove", (event) => {
-    let x = eye.left + eye.x / 2;
-    let y = eye.top + eye.y / 2;
-    let rad = Math.atan2(event.x - x, event.x - y);
-    let rot = rad * (180 / Math.PI) * -1 + 180;
-    eye.style.webkit = "rotate(" + rot + "deg)";
-    eye.style.moz = "rotate(" + rot + "deg)";
-    eye.style.ms = "rotate(" + rot + "deg)";
-    eye.style.transform = "rotate(" + rot + "deg)";
-  });
-});
-*/
