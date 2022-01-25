58a85001c8048aa2f211a05c929e3d2d3910bfbe
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2022-01-24 23:12:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // nav bar effect on scroll
-
 window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
   navbar.classList.toggle("static", window.scrollY > 0);
 });
+
 
 //ham menu open and close effect-responsive design
 const hamIcon = document.getElementById("ham-icon");
@@ -11,7 +11,7 @@ const hamMenu = document.getElementById("ham-menu");
 const navbarLinks = document.querySelector(".navbar__links");
 const hamMenuItems = document.querySelectorAll(".navbar__links a");
 const sendBtn = document.querySelector(".send-btn");
-const eyes = document.querySelectorAll(".eye");
+
 
 function verifyActiveMenu() {
   if (hamMenu.classList.contains("active")) {
@@ -21,11 +21,13 @@ function verifyActiveMenu() {
   }
 }
 
+
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   navbarLinks.classList.toggle("active");
   verifyActiveMenu();
 });
+
 
 hamMenuItems.forEach((item) => {
   item.addEventListener("click", () => {
